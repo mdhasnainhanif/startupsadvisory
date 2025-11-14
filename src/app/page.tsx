9d334ec9e662c91a53d7831e278dfff8fd6efb5c
Safif2000 <s.mafif222@gmail.com>
2025-11-14 20:45:14 +0500

    slidersection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroBanner from "./components/HeroBanner/HeroBanner";
-import { HERO_BANNER } from "./constants";
+import { HERO_BANNER, GROWTH_GUIDES } from "./constants";
+import GrowthSlider from "./components/GrowthSlider/GrowthSlider";
 
 export default function Home() {
   return (
     <main className="relative">
       <HeroBanner {...HERO_BANNER} />
+      <GrowthSlider items={GROWTH_GUIDES} />
     </main>
   );
 }
